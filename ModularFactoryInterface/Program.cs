e8fde389c298b11bc3340cb705b323f34342df69
Kevin Yako <kevin.yako@skf.com>
2023-03-30 13:56:52 +0200

    Changed URL for docker

--- ModularFactoryInterface/Program.cs
+++ ModularFactoryInterface/Program.cs
@@ -11,12 +11,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped<HubConnection>(e => new HubConnectionBuilder().WithUrl($"http://172.17.14.61/ConfigureSvgHub").Build());
-builder.Services.AddScoped<HubConnection>(e => new HubConnectionBuilder().WithUrl($"https://localhost:7235/ConfigureSvgHub").Build());
+builder.Services.AddScoped<HubConnection>(e => new HubConnectionBuilder().WithUrl($"http://172.17.14.61/ConfigureSvgHub").Build());
+//builder.Services.AddScoped<HubConnection>(e => new HubConnectionBuilder().WithUrl($"https://localhost:7235/ConfigureSvgHub").Build());
 builder.Services.AddHttpClient<ISvgService, SvgService>(httpClient =>
 {
-//    httpClient.BaseAddress = new Uri("http://172.17.14.61/");
-    httpClient.BaseAddress = new Uri("https://localhost:7235/");
+    httpClient.BaseAddress = new Uri("http://172.17.14.61/");
+//    httpClient.BaseAddress = new Uri("https://localhost:7235/");
 });
 builder.Services.AddMudServices();
 
